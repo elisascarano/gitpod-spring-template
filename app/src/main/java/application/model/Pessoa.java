94f16a58647e68af1fb6e5565e81d4adae302da5
elisascarano <elisatscarano@gmail.com>
2024-09-06 12:25:55 +0000

    aula06/09

---
+++ app/src/main/java/application/model/Pessoa.java
@@ -0,0 +1,12 @@
+package application.model;
+
+public class Pessoa {
+    private long id;
+    private String nome;
+    private int idade;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {}
+}
